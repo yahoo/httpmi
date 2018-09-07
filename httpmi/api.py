2d8e4b3fd4dfe06c2a9c6052745708a00a783ef7
Jim Rollenhagen <jim@jimrollenhagen.com>
2018-09-07 15:41:30 -0400

    refactor how we deal with credentials

--- httpmi/api.py
+++ httpmi/api.py
@@ -7,11 +7,15 @@ from httpmi import ipmi
 
 app = Flask(__name__)
 
+CREDS_KEYS = ['bmc', 'user', 'password']
+
+
+def _get_bmc_credentials():
+    return {k: request.form[k] for k in CREDS_KEYS}
+
 
 @app.route('/power')
 def power():
+    creds = _get_bmc_credentials()
     if request.method == 'GET':
-        bmc = request.form['bmc']
-        user = request.form['user']
-        password = request.form['password']
-        return jsonify({'state': ipmi.get_power(bmc, user, password)})
+        return jsonify({'state': ipmi.get_power(creds)})
--- httpmi/ipmi.py
+++ httpmi/ipmi.py
@@ -1,9 +1,11 @@
 from pyghmi.ipmi import command
 
 
-def _connect(bmc, user, password):
-    return command.Command(bmc=bmc, userid=user, password=password)
+def _connect(credentials):
+    return command.Command(bmc=credentials['bmc'],
+                           userid=credentials['user'],
+                           password=credentials['password'])
 
 
-def get_power(bmc, user, password):
-    return _connect(bmc, user, password).get_power()['powerstate']
+def get_power(credentials):
+    return _connect(credentials).get_power()['powerstate']
